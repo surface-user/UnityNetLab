692522683015ace933344a444ab064c474374166
surface-user <1692115779@qq.com>
2026-09-14 20:26:41 +0800

    feat:基础TcpServer
    初步写了TcpServer类中的Start()和Ondestroy(),
    分别定义了创建监听器和销毁监听器.

---
+++ Assets/Scripts/Networking/TcpServer.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+class TcpServer : MonoBehaviour{
+    private TcpListener tcplistener;
+    //创建并启动监听器
+    private void Start(){
+       IPAddress ipaddress = IPAddress.Loopback;
+        int port = 7777;
+        tcplistener = new TcpListener(ipaddress, port);
+        tcplistener.Start();
+        Debug.Log("Server 开始监听");
+    }
+    //在游戏销毁时停止监听
+    private void OnDestroy(){
+        if(tcplistener !=null){
+            tcplistener.Stop();
+            Debug.Log("Server 停止监听");
+            // Debug.Log 是 Unity 提供的调试输出方法，用于在 Console 控制台打印普通调试信息；
+            //参数支持字符串、变量、对象等任意可转字符串的内容，常用于运行时排查逻辑、查看状态
+        }
+    }
+}
