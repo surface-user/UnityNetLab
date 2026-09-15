171250d6ad34d8f1c07a50835b26d1a2c708c0c7
surface-user <1692115779@qq.com>
2026-09-15 16:31:46 +0800

    Feat:添加update()
    今天有事,仅做了Update()函数的添加,目前最小的服务监听器做好了

--- Assets/Scripts/Networking/TcpServer.cs
+++ Assets/Scripts/Networking/TcpServer.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 class TcpServer : MonoBehaviour{
     private TcpListener tcplistener;
+    private TcpClient connectedClient;
     //创建并启动监听器
     private void Start(){
        IPAddress ipaddress = IPAddress.Loopback;
         int port = 7777;
-        tcplistener = new TcpListener(ipaddress, port);
-        tcplistener.Start();
-        Debug.Log("Server 开始监听");
+        if(tcplistener == null){
+            tcplistener = new TcpListener(ipaddress, port);
+            tcplistener.Start();
+            Debug.Log("Server 开始监听");
+        }
     }
     //在游戏销毁时停止监听
     private void OnDestroy(){
@@ -20,5 +23,17 @@ class TcpServer : MonoBehaviour{
             // Debug.Log 是 Unity 提供的调试输出方法，用于在 Console 控制台打印普通调试信息；
             //参数支持字符串、变量、对象等任意可转字符串的内容，常用于运行时排查逻辑、查看状态
         }
+        if(connectedClient!=null){
+            connectedClient.Close();
+            Debug.Log("Server 关闭客户端连接");
+        }
+    }
+
+    private void Update(){
+         if(connectedClient!=null) return;
+         if(!tcplistener.Pending()) return;
+         connectedClient=tcplistener.AcceptTcpClient();
+         Debug.Log("Server 接受客户端连接");
     }
 }
+
